8b9d537094e96c962906538d79f2227837ecb8f0
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-02-27 19:56:27 +0700

    moved to staging letsencrypt and inital cert creation at startup

--- create_update_cert.sh
+++ create_update_cert.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-echo "* * * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
+echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
 crond -c /etc/crontabs -f &
 nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;'
+/dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
