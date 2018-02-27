1bfa9cca1cb3ac27104196a7eef53c9238916070
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-02-27 19:26:20 +0700

    fixed crontab

--- create_update_cert.sh
+++ create_update_cert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "* * * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf > /dev/pts/1" >> /etc/crontabs/root
+echo "* * * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
 crond -c /etc/crontabs -f &
 nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;'
