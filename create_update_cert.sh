a2b67c5d9e730f9f45f4887fd1c3e9fe9aa8f1ec
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-02-27 18:55:34 +0700

    testing cron in alpine image

--- create_update_cert.sh
+++ create_update_cert.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+echo "* * * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf > > /dev/pts/1" >> /etc/crontabs/root
+crond
 nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;'
-/dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
