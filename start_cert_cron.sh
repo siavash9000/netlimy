461d251e0072c38c2551852ccb5f47c5c87b4872
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-02-27 10:12:43 +0700

    fixed config path in start_cert_cron.sh

--- start_cert_cron.sh
+++ start_cert_cron.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;' &
-/dehydrated/dehydrated --register --accept-terms --config /dehydrated/config/
-/dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/
+/dehydrated/dehydrated --register --accept-terms --config /dehydrated/config/conf
+/dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
