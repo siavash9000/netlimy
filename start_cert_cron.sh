bfcd8b2317cf9ad491dad2a3e2e26e66db5875ed
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-02-27 11:45:44 +0700

    set letsencrypt to staging to prevent rate limit

--- start_cert_cron.sh
+++ start_cert_cron.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;' &
-/dehydrated/dehydrated --register --accept-terms --config /dehydrated/config/conf
 /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
