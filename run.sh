d81da318aea57823c54222d207f87764eac7df9b
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-01 21:25:54 +0700

    added echo for debugging in production

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "PRODUCTION=$PRODUCTION"
 if [ "$PRODUCTION" = "true" ] ; then
+    echo "performing PRODUCTION stuff"
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
     nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;'
