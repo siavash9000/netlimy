fff8679dee47f995b4c06e634853b49efd49e875
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-01 21:30:51 +0700

    fix tryout

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "PRODUCTION=$PRODUCTION"
-if [ "$PRODUCTION" = "true" ] ; then
+if [ "$PRODUCTION" == "true" ] ; then
     echo "performing PRODUCTION stuff"
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
