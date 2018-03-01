29423433381f87f6331fc95fe45b6502652d7638
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-01 20:17:54 +0700

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 if [ "$PRODUCTION" = true ] ; then
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
-    nginx -c /etc/nginx/nginx_dehydrated.conf -g 'daemon off;'
+    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;'
     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
 fi
 /website_updater.sh
