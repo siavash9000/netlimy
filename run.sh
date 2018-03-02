ba299d10d3ad687ee2eafb81aef94bdb5e4746f3
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-02 11:20:09 +0700

    fixed runs.h

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "PRODUCTION=$PRODUCTION"
 if [ "$PRODUCTION" == "true" ] ; then
-    echo "adding dehydrated tor cron"
+    echo "adding dehydrated to cron"
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
     echo "starting nginx"
-    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;'
+    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;' &
     echo "creating /domains.txt"
     echo "$DOMAINS" > /domains.txt
     echo "starting dehydrated"
