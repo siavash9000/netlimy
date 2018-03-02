56c9d606139cb9143dd6814732f69cb37370de87
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-02 11:16:45 +0700

    switched to live letsencrypt

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ "$PRODUCTION" == "true" ] ; then
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
     echo "starting nginx"
-    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;' &
+    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;'
     echo "creating /domains.txt"
     echo "$DOMAINS" > /domains.txt
     echo "starting dehydrated"
