14d5bb444dfdb2bde24db4e5aceaef6e430c629a
Siavash Sefidrodi <sefidrodi@googlemail.com>
2018-03-02 11:04:41 +0700

    added more logging to run.sh, starting nginx in background

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ if [ "$PRODUCTION" == "true" ] ; then
     echo " 0 24 * * *     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf" >> /etc/crontabs/root
     crond -c /etc/crontabs -f &
     echo "starting nginx"
-    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;'
+    nginx -c /dehydrated/config/nginx_dehydrated.conf -g 'daemon off;' &
+    echo "creating /domains.txt"
     echo "$DOMAINS" > /domains.txt
+    echo "starting dehydrated"
     /dehydrated/dehydrated --cron --accept-terms --config /dehydrated/config/conf
 fi
 /website_updater.sh
